4c07964970fc61a283043d9b9ebae12d53431f29
Peter Olujimi <91281036+debolujimi@users.noreply.github.com>
2026-09-23 15:43:56 +0200

    Add demo/agents/__init__.py

---
+++ demo/agents/__init__.py
@@ -0,0 +1,13 @@
+from .customer import CustomerEngagementAgent
+from .demand import DemandSensingAgent
+from .inventory import InventoryManagementAgent
+from .pricing import PricingAgent
+from .procurement import ProcurementAgent
+
+__all__ = [
+    "DemandSensingAgent",
+    "InventoryManagementAgent",
+    "ProcurementAgent",
+    "PricingAgent",
+    "CustomerEngagementAgent",
+]
